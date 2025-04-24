b93138c1b615f56836d47a1e8adc7cdb7bbe991b
aksh10patil <rajdeeppatil2021@gmail.com>
2025-04-24 19:47:19 +0530

    cors issue

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://www.alpixn.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   }));
