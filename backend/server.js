95a1539d598ecd3ff29da9f197a4339caf342d69
aksh10patil <rajdeeppatil2021@gmail.com>
2025-04-26 22:35:55 +0530

    add /api

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api',internshipRoutes);
 
 // Basic route
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Internship API is running');
 });
 
